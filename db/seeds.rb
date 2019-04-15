0ebfa22d5d5cd6ccf181312f216c1c8b2b0fa206
Scott Goyette <scott.goyette@rubypythmore.com>
2019-04-15 11:25:24 -0400

    seeded document data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+10.times do |d|
+  FreelanceDocument.create!(
+    title: "Document #{d}",
+    description: 'Direct trade literally photo booth distillery chicharrones, hot chicken squid prism thundercats. Yr pitchfork adaptogen messenger bag hot chicken la croix actually photo booth bicycle rights flexitarian pop-up salvia drinking vinegar whatever. Small batch four dollar toast chillwave YOLO bushwick post-ironic tousled. Activated charcoal tacos cloud bread, forage glossier paleo schlitz green juice chartreuse. Disrupt cray kitsch irony copper mug succulents normcore lumbersexual aesthetic tousled. Synth lumbersexual yr, shabby chic mumblecore pinterest gentrify roof party four loko twee butcher raclette literally flexitarian. Thundercats paleo copper mug flannel snackwave hashtag blue bottle polaroid banh mi meh post-ironic banjo hoodie.',
+    file_url: 'https://docs.google.com/document/d/1T5aDhT-WWDcy9RZvnnJe5ohpsLM9ggF6wd6twrWOqR4/edit?usp=sharing',
+    image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg',
+  )
+end
